4fa2608da085365cfb174933074ed4d5ac7f7be1
sabirqureshi104-creator <sabirqureshi104@gmail.com>
2026-07-07 02:41:08 +0200

    Create Admin Area foundation

---
+++ Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auction_web.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class DashboardController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -35,6 +35,10 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
