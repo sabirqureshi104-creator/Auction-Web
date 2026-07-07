3cdd8465bde6c0f5c575bac784725997d135786a
sabirqureshi104-creator <sabirqureshi104@gmail.com>
2026-07-07 03:07:18 +0200

    Add admin users module with search and block controls

---
+++ Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,74 @@
+using Auction_web.Areas.Admin.Models;
+using Auction_web.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auction_web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(string? search)
+        {
+            var query = _context.Users
+                .Include(u => u.ItemsForSale)
+                .Include(u => u.Bids)
+                .AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                search = search.Trim();
+
+                query = query.Where(u =>
+                    u.Username.Contains(search) ||
+                    u.Email.Contains(search) ||
+                    (u.FullName != null && u.FullName.Contains(search)));
+            }
+
+            var model = new UsersViewModel
+            {
+                Search = search,
+                Users = await query.OrderByDescending(u => u.CreatedDate).ToListAsync()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Block(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user != null)
+            {
+                user.IsBlocked = true;
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Unblock(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user != null)
+            {
+                user.IsBlocked = false;
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
