84bb7412cf04c9a5a316d4d2858eb9acb4450f25
sabirqureshi104-creator <sabirqureshi104@gmail.com>
2026-07-07 03:07:18 +0200

    Add admin users module with search and block controls

---
+++ Areas/Admin/Models/CategoriesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Auction_web.Models;
+
+namespace Auction_web.Areas.Admin.Models
+{
+    public class CategoriesViewModel
+    {
+        public string? Search { get; set; }
+        public List<Category> Categories { get; set; } = new();
+    }
+}
---
+++ Areas/Admin/Models/UsersViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Auction_web.Models;
+
+namespace Auction_web.Areas.Admin.Models
+{
+    public class UsersViewModel
+    {
+        public string? Search { get; set; }
+        public List<User> Users { get; set; } = new();
+    }
+}
