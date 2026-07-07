a5d5c9baf726517360259e3b0165418a8bcb6ec5
sabirqureshi104-creator <sabirqureshi104@gmail.com>
2026-07-07 02:54:21 +0200

    Add admin dashboard layout and live statistics

--- Areas/Admin/Controllers/DashboardController.cs
+++ Areas/Admin/Controllers/DashboardController.cs
@@ -1,13 +1,49 @@
+using Auction_web.Areas.Admin.Models;
+using Auction_web.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Auction_web.Areas.Admin.Controllers
 {
-	[Area("Admin")]
-	public class DashboardController : Controller
-	{
-		public IActionResult Index()
-		{
-			return View();
-		}
-	}
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DashboardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = new DashboardViewModel
+            {
+                TotalUsers = await _context.Users.CountAsync(),
+                TotalCategories = await _context.Categories.CountAsync(),
+                TotalItems = await _context.Items.CountAsync(),
+                ActiveAuctions = await _context.Items.CountAsync(x => x.BidStatus == "A"),
+                TotalBids = await _context.Bids.CountAsync(),
+
+                RecentUsers = await _context.Users
+                    .OrderByDescending(x => x.CreatedDate)
+                    .Take(5)
+                    .ToListAsync(),
+
+                RecentItems = await _context.Items
+                    .OrderByDescending(x => x.CreatedDate)
+                    .Take(5)
+                    .ToListAsync(),
+
+                RecentBids = await _context.Bids
+                    .Include(x => x.Item)
+                    .Include(x => x.Buyer)
+                    .OrderByDescending(x => x.BidDate)
+                    .Take(5)
+                    .ToListAsync()
+            };
+
+            return View(model);
+        }
+    }
 }
---
+++ Areas/Admin/Models/DashboardViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Auction_web.Models;
+
+namespace Auction_web.Areas.Admin.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalUsers { get; set; }
+        public int TotalCategories { get; set; }
+        public int TotalItems { get; set; }
+        public int ActiveAuctions { get; set; }
+        public int TotalBids { get; set; }
+
+        public List<User> RecentUsers { get; set; } = new();
+        public List<Item> RecentItems { get; set; } = new();
+        public List<Bid> RecentBids { get; set; } = new();
+    }
+}
